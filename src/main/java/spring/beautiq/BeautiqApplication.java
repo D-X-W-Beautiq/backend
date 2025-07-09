2ac2bfd3e9d102192571bcbd7f3e4bd5d83d6036
Minu <46252059+alsdn4956@users.noreply.github.com>
2025-07-10 00:46:47 +0900

    Initial setting

---
+++ src/main/java/spring/beautiq/BeautiqApplication.java
@@ -0,0 +1,13 @@
+package spring.beautiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautiqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautiqApplication.class, args);
+    }
+
+}
