e6ecc035c7e98f222aaafe884cc25277eb81a33f
Minu <46252059+alsdn4956@users.noreply.github.com>
2025-07-10 00:50:32 +0900

    Initial setting

--- src/main/java/spring/beautiq/BeautiqApplication.java
+++ src/main/java/spring/beautiq/BeautiqApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeautiqApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BeautiqApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BeautiqApplication.class, args);
+	}
 
 }
--- src/test/java/spring/beautiq/BeautiqApplicationTests.java
+++ src/test/java/spring/beautiq/BeautiqApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BeautiqApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
