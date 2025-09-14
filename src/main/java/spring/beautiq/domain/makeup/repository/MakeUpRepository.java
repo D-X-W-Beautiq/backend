dd80848114acb1998dc22e4dc485dda5ee59903b
clzlol <nyc0527@naver.com>
2025-09-14 17:28:59 +0900

    fix: 객체 id가 Long이 아니라  UUID이므로 Repository 부분 수정

--- src/main/java/spring/beautiq/domain/makeup/repository/MakeUpRepository.java
+++ src/main/java/spring/beautiq/domain/makeup/repository/MakeUpRepository.java
@@ -3,5 +3,7 @@ package spring.beautiq.domain.makeup.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.beautiq.domain.makeup.entity.MakeUp;
 
-public interface MakeUpRepository extends JpaRepository<MakeUp, Long> {
+import java.util.UUID;
+
+public interface MakeUpRepository extends JpaRepository<MakeUp, UUID> {
 }
