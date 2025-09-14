6bde70d66b1b31c98bd5a2264273fe6647995e69
clzlol <nyc0527@naver.com>
2025-09-14 17:30:17 +0900

    fix: id가 UUID이므로 타입 수정

--- src/main/java/spring/beautiq/domain/makeup/s3/S3Service.java
+++ src/main/java/spring/beautiq/domain/makeup/s3/S3Service.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class S3Service {
@@ -23,7 +24,7 @@ public class S3Service {
     /**
      * S3에 이미지 업로드 하기
      */
-    public String uploadImage(MultipartFile image, Long id) throws IOException {
+    public String uploadImage(MultipartFile image, UUID id) throws IOException {
         String fileName = id.toString(); // 고유한 파일 이름 생성
 
         // 메타데이터 설정
