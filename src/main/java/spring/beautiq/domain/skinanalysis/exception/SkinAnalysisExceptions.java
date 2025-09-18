b1b686261cc3332953fc767c0b4eb5bbb59e7f62
BORB <qzsec299@naver.com>
2025-09-18 21:26:11 +0900

    feat(skinanalysis exception): 에러 케이스 추가

--- src/main/java/spring/beautiq/domain/skinanalysis/exception/SkinAnalysisExceptions.java
+++ src/main/java/spring/beautiq/domain/skinanalysis/exception/SkinAnalysisExceptions.java
@@ -11,7 +11,8 @@ import java.util.function.Supplier;
 @RequiredArgsConstructor
 public enum SkinAnalysisExceptions implements ErrorCode {
     SKIN_ANALYSIS_NOT_FOUND("피부 분석 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    SKIN_ANALYSIS_FORBIDDEN("피부 분석 정보에 접근할 수 있는 권한이 없습니다.", HttpStatus.FORBIDDEN)
+    SKIN_ANALYSIS_FORBIDDEN("피부 분석 정보에 접근할 수 있는 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    AI_SERVER_RESPONSE_EMPTY("AI 서버 응답이 비어 있습니다.", HttpStatus.INTERNAL_SERVER_ERROR)
     ;
 
     private final String message;
