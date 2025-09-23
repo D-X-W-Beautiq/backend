2dc076b2ab7db9db994f82fb016cddb2c8bad9cf
BORB <qzsec299@naver.com>
2025-09-24 03:18:10 +0900

    feat(skinAnalysis dto): 일별 조회 응답 dto

---
+++ src/main/java/spring/beautiq/domain/skinanalysis/dto/response/DailySkinDatesResponse.java
@@ -0,0 +1,31 @@
+package spring.beautiq.domain.skinanalysis.dto.response;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import spring.beautiq.domain.skinanalysis.entity.SkinAnalysis;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DailySkinDatesResponse {
+
+    private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+
+    @NotNull
+    private List<String> skinAnalysisDates;
+
+    public static DailySkinDatesResponse from(List<SkinAnalysis> skinAnalyses) {
+        return DailySkinDatesResponse.builder()
+                .skinAnalysisDates(skinAnalyses.stream()
+                        .sorted(Comparator.comparing(SkinAnalysis::getCreatedAt))
+                        .map(sa -> sa.getCreatedAt().format(FMT))
+                        .toList())
+                .build();
+    }
+}
