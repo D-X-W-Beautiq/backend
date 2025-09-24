ddb0b7c8cfc95513875ac0d3ca8fcc1612af7511
BORB <qzsec299@naver.com>
2025-09-25 00:20:55 +0900

    fix(product dto): JSON 역직렬화 실패 가능성 있음, @NoArgsConstructor 누락 문제 해결

--- src/main/java/spring/beautiq/domain/product/dto/request/ProductRequest.java
+++ src/main/java/spring/beautiq/domain/product/dto/request/ProductRequest.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import spring.beautiq.domain.product.dto.common.ProductFilter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ProductRequest {
 
