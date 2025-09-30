dd6a4486e59cc54b4eac3758b81b864cd1e7215f
clzlol <nyc0527@naver.com>
2025-09-30 15:51:39 +0900

    feat: 이미지 이름으로 메이크업 조회하기 추가. 이미지 이름 또한 UUID로 되어있어 중복 가능성이 낮아 실질적으로 안전해서 메이크업 조회에 imageName으로 조회하기 사용

--- src/main/java/spring/beautiq/domain/makeup/repository/MakeUpRepository.java
+++ src/main/java/spring/beautiq/domain/makeup/repository/MakeUpRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import spring.beautiq.domain.makeup.entity.MakeUp;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MakeUpRepository extends JpaRepository<MakeUp, UUID> {
     List<MakeUp> findAllByUserId(UUID userId);
+    Optional<MakeUp> findByImageName(String imageName);
 }
