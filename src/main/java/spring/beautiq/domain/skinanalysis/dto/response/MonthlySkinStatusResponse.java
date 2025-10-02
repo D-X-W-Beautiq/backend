4934e5b0106bf5edb56ce3f379fcb4eebaaba9d0
BORB <qzsec299@naver.com>
2025-10-03 05:01:48 +0900

    refactor(skinAnalysis dto): monthly, daily의 코드 리팩토링

--- src/main/java/spring/beautiq/domain/skinanalysis/dto/response/MonthlySkinStatusResponse.java
+++ src/main/java/spring/beautiq/domain/skinanalysis/dto/response/MonthlySkinStatusResponse.java
@@ -2,23 +2,34 @@ package spring.beautiq.domain.skinanalysis.dto.response;
 
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import spring.beautiq.domain.skinanalysis.dto.common.SkinStatusHistory;
 
 import java.util.List;
 
-@ArraySchema(
-    schema = @Schema(implementation = SkinStatusHistory.class),
-    minItems = 0,
-    maxItems = 31
-)
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(
-    description = "월별 피부 상태 이력 배열",
-    example = "[{\"skinStatus\":\"GOOD\",\"dayDate\":\"2025-09-06\"},{\"skinStatus\":\"CAUTION\",\"dayDate\":\"2025-09-07\"},{\"skinStatus\":\"DANGER\",\"dayDate\":\"2025-09-08\"}]",
-    type = "array"
+    description = "월별 피부 상태 이력 응답 래퍼",
+    example = "{\n  \"monthlyHistory\": [\n    {\"skinStatus\":\"GOOD\",\"dayDate\":\"2025-09-06\"},\n    {\"skinStatus\":\"CAUTION\",\"dayDate\":\"2025-09-07\"}\n  ]\n}"
 )
 public class MonthlySkinStatusResponse {
 
-    public static List<SkinStatusHistory> of(List<SkinStatusHistory> skinStatusHistories) {
-        return skinStatusHistories;
+    @NotNull
+    @Valid
+    @ArraySchema(
+        schema = @Schema(implementation = SkinStatusHistory.class),
+        minItems = 0,
+        maxItems = 31
+    )
+    private List<SkinStatusHistory> monthlyHistory;
+
+    public static MonthlySkinStatusResponse of(List<SkinStatusHistory> list) {
+        return MonthlySkinStatusResponse.builder().monthlyHistory(list).build();
     }
 }
