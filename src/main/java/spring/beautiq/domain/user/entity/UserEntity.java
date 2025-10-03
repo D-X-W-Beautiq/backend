489fb318e821da69248772dacd7c75802b11386b
Minu <46252059+alsdn4956@users.noreply.github.com>
2025-10-04 00:28:42 +0900

    Enhance: OAuth2 success handler principal support

--- src/main/java/spring/beautiq/domain/user/entity/UserEntity.java
+++ src/main/java/spring/beautiq/domain/user/entity/UserEntity.java
@@ -1,17 +1,10 @@
 package spring.beautiq.domain.user.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.util.Set;
-
 import java.util.UUID;
 import lombok.*;
 import spring.beautiq.global.base.BaseEntity;
