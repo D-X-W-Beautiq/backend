49aa75ee7cc556e219e8799b9eb1c6fd0a641725
clzlol <nyc0527@naver.com>
2025-11-05 23:38:54 +0900

    fix: 조회 시 사용자 권한 검증

--- src/main/java/spring/beautiq/domain/makeup/MakeUpService.java
+++ src/main/java/spring/beautiq/domain/makeup/MakeUpService.java
@@ -117,6 +117,10 @@ public class MakeUpService {
             throw new RuntimeException("Access denied: not your makeup");
         }
 
+        if(!makeUpEntity.getUser().getId().equals(userId)) { // 조회 시 사용자 권한 검증
+            throw new RuntimeException("Unauthorized");
+        }
+
         return MakeUptoMakeUpDetailResponseDto(makeUpEntity);
     }
 
