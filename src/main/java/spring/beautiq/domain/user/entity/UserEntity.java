326ba368839682be0877f7bd6004b7e21fd14511
BORB <qzsec299@naver.com>
2025-11-14 00:13:46 +0900

    feat: CASCADE 설정

--- src/main/java/spring/beautiq/domain/user/entity/UserEntity.java
+++ src/main/java/spring/beautiq/domain/user/entity/UserEntity.java
@@ -1,12 +1,18 @@
 package spring.beautiq.domain.user.entity;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import spring.beautiq.domain.product.wishlist.entity.WishlistProductEntity;
+import spring.beautiq.domain.skinanalysis.entity.SkinAnalysisEntity;
 import spring.beautiq.global.base.BaseEntity;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -27,4 +33,9 @@ public class UserEntity extends BaseEntity {
     @Column(length = 2048)
     private String profileImage;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<SkinAnalysisEntity> skinAnalyses;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<WishlistProductEntity> wishlistProducts;
 }
